cbe599bbe93a3ecf211b2eb449519d3a14e733bb
root <root@localhost.localdomain>
2018-11-19 11:31:32 -0500

    log in tester

--- tester.py
+++ tester.py
@@ -4,8 +4,9 @@ from abc import ABC, abstractmethod
 
 class Tester:
    
-    def __init__(self, response_condition):
+    def __init__(self, response_condition, log = False):
         self.response_condition = response_condition
+        self.log = log
 
     @abstractmethod
     def get_request(payload):
@@ -14,6 +15,9 @@ class Tester:
 
     def test(self, payload):
         r = self.get_request(payload)
+        if self.log:
+            print("[*] request {}".format(r.url))
+            print("[*] response: {}".format(r.text))
         return self.response_condition(r)
         
 
