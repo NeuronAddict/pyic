4841cb443fa9daf5421a0a76add1500f556170a6
root <root@localhost.localdomain>
2018-11-20 10:37:06 -0500

    SqliEncoder + char encoding technique.

--- test_tools.py
+++ test_tools.py
@@ -1,8 +1,21 @@
+import unittest
 from unittest import TestCase
 from tools import *
 
 
 class TestEncode_str(TestCase):
-    def test_encode_str(self):
+
+    def test_encode_hexa_legacy(self):
         self.assertEqual('0x41', encode_str('A'))
         self.assertEqual('0x2f6574632f706173737764', encode_str('/etc/passwd'))
+
+    def test_encode_hexa(self):
+        self.assertEqual('0x41', SqliEncoder.str_to_hexa('A'))
+        self.assertEqual('0x2f6574632f706173737764', SqliEncoder.str_to_hexa('/etc/passwd'))
+
+    def test_encode_char(self):
+        self.assertEqual('CHAR(77,121,83,81,76)', SqliEncoder.str_to_char('MySQL'))
+    
+
+if __name__ == '__main__':
+    unittest.main()
--- tools.py
+++ tools.py
@@ -1,6 +1,27 @@
 def encode_str(sql_str):
     return '0x' + sql_str.encode('ascii').hex()
 
-
 def es(sql_str):
     return encode_str(sql_str)
+
+class SqliEncoder:
+    
+    def str_to_hexa(sql_str):
+        return '0x' + sql_str.encode('ascii').hex()
+
+    def str_to_char(sql_str):
+        """ Encode the string with the CHAR technique.
+
+        'MySQL' become CHAR(77,121,83,81,'76').
+        see https://dev.mysql.com/doc/refman/8.0/en/string-functions.html#function_char
+        """
+        ret = 'CHAR('
+        i = 1
+        for c in sql_str:
+            ret += str(ord(c))
+            if i != len(sql_str):
+                ret += ','
+            i += 1
+        return ret + ')'
+
+
