075572cc470bed821f726fe583bbbb8eb7cf1af2
manu <dev@lacourarie.org>
2018-11-21 01:41:40 +0100

    Help message on statup.

--- pysqli/__init__.py
+++ pysqli/__init__.py
@@ -6,3 +6,6 @@ from pysqli.db_dumper import DbDumper
 
 if __name__ == '__main__':
     print("#" * 25 + "\n#\n# python sqli lib loaded!\n#\n" + "#" * 25)
+    print('\nTo get help, type help(object) with object in Tester, StringFinder, SqliEncoder, DbDumper, HasText '
+          'or Not\n')
+
