303d084eda2b129999a2b6636cd39e2ee87dfc26
manu <dev@lacourarie.org>
2018-11-21 01:38:55 +0100

    Document tester.

--- pysqli/tester.py
+++ pysqli/tester.py
@@ -1,11 +1,26 @@
 class Tester:
-
+    """
+    A Tester can test if a sqli injected query return true or false (its a blind tester).
+    TODO : rename to BlindTester
+    """
     def __init__(self, request_builder, response_condition, log=False):
         """
+        Create e new Blind tester. To do this, we need :
+        - a query, make with requests from a payload
+        - a condition, to determine if the result is True from a response
+
+       they can be lambda, function or callable objects
+       - The query take a String payload to return a requests response
+       - The condition take a response to return a boolean
+
+        example :
+        tester = Tester(
+                    lambda payload : requests.get('http://127.0.0.1/inject.php', params={'id': '1 {}'.format(payload)}),
+                    lambda r: 'error' not in r.text)
 
         :param request_builder: callable that take a payload to return a response
         :param response_condition: callable that take a response and return a boolean
-        :param log:
+        :param log: if True, log all requests / response (very verbose). False by default.
         """
         self.response_condition = response_condition
         self.log = log
