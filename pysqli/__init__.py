5dc72e860f4b83b1220c054844ec33313f067bc3
manu <dev@lacourarie.org>
2018-11-21 01:25:32 +0100

    add init file.

---
+++ pysqli/__init__.py
@@ -0,0 +1,8 @@
+from pysqli.tester import Tester
+from pysqli.tools import SqliEncoder
+from pysqli.body_conditions import *
+from pysqli.string_finder import StringFinder
+from pysqli.db_dumper import DbDumper
+
+if __name__ == '__main__':
+    print("#" * 25 + "\n#\n# python sqli lib loaded!\n#\n" + "#" * 25)
