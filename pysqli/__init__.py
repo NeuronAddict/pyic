07d29bb6ae451d6d9641acc6a66cc82c523b57f8
manu <dev@lacourarie.org>
2018-11-21 01:51:04 +0100

    Import requests in init.

--- pysqli/__init__.py
+++ pysqli/__init__.py
@@ -4,6 +4,9 @@ from pysqli.body_conditions import *
 from pysqli.string_finder import StringFinder
 from pysqli.db_dumper import DbDumper
 
+# noinspection PyUnresolvedReferences
+import requests
+
 if __name__ == '__main__':
     print("#" * 25 + "\n#\n# python sqli lib loaded!\n#\n" + "#" * 25)
     print('\nTo get help, type help(object) with object in Tester, StringFinder, SqliEncoder, DbDumper, HasText '
