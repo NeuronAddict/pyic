2413cf7276ad56ecc2dc3213420d9be6fd6309f1
neuronaddict <dev@lacourarie.org>
2018-11-22 15:41:11 +0100

    Optional offset in StarExtract.

--- pysqli/star_extract.py
+++ pysqli/star_extract.py
@@ -1,11 +1,15 @@
 
 class StarExtract:
-    def __init__(self, pattern):
+    def __init__(self, pattern, shift=0):
         self.pattern = pattern.split('*')
+        self.shift = shift
         if len(self.pattern) != 2:
             raise Exception('pattern of StarExtract must contain only one star (*)')
 
     def __call__(self, response):
-        begin = response.text.find(self.pattern[0]) + len(self.pattern[0])
-        end = response.text[begin:].find(self.pattern[1]) + begin
-        return response.text[begin:end]
+        text = response.text[self.shift:]
+        print('-' * 45)
+        print(text)
+        begin = text.find(self.pattern[0]) + len(self.pattern[0])
+        end = text[begin:].find(self.pattern[1]) + begin
+        return text[begin:end]
--- pysqli/test_starExtract.py
+++ pysqli/test_starExtract.py
@@ -13,3 +13,7 @@ class TestStarExtract(TestCase):
     def test_StartExtract(self):
         self.assertEqual('pattern', StarExtract('blah</h2><h2>*</h2>')
                          .__call__(MockResponse('<h2>blah</h2><h2>pattern</h2>blah')))
+
+    def test_StartExtract_withshift(self):
+        self.assertEqual('pattern', StarExtract('<h2>*</h2>', 8)
+                         .__call__(MockResponse('<h2>blah</h2><h2>pattern</h2>blah')))
