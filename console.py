682b30664109315875d4b55b0447b6efd1e7c23e
neuronaddict <dev@lacourarie.org>
2018-11-25 18:53:23 +0100

    Working console trying.

--- console.py
+++ console.py
@@ -7,36 +7,56 @@ s = '.' * 100
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
 
+
 def delete_last_lines(n=1):
-    for _ in range(n):
-        sys.stdout.write(CURSOR_UP_ONE)
-        sys.stdout.write(ERASE_LINE)
-
-
-def update(str_, newline_count):
-    if len(s) != l:
-        raise Exception("Length error : " + str_)
-    time.sleep(1.5)
-
-    delete_last_lines(newline_count + 1)
-    sys.stdout.write(str_ + '\n')
+    sys.stdout.write((CURSOR_UP_ONE + ERASE_LINE) * n)
     sys.stdout.flush()
 
-sys.stdout.write(s)
-sys.stdout.flush()
+
+def test():
+    print('.' * 100)
+    time.sleep(.5)
+    print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+    print('A' * 49 + '\n' + '.' * 50)
+    time.sleep(.5)
+    print((CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE) * 2)
+    print('A' * 49 + '\n' + 'B' * 49 + '\n')
 
 
-s = 'A' * 20 + '.' * 30  + '\n' + '.' * 49
-update(s, 0)
 
-s = 'A' * 20 + '\n' + 'B' * 29 + '\n' + '.' * 49
-update(s, 1)
+CURSOR_UP_ONE = '\x1b[1A'  # <--Replace with '\033[F' if you don't have VT100
+ERASE_LINE = '\x1b[2K'  # <--Replace with '\033[K' if you don't have VT100
 
-s = 'A' * 20 + '\n' + 'B' * 29 + '\n' + 'A' * 20 + '\n' + 'C' * 27 + '\n'
-update(s, 2)
+def close_garage_dors():
+    a = '_ _ _'
+    for i in range(5):
+        time.sleep(.5)
+        print(a)
+    for b in range(5):
+        time.sleep(.5)
+        print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+
+#close_garage_dors()
 
 
-print('\n\n\n' + s)
+def update_screen(old_str, new_str):
+    delete_last_lines(old_str.count('\n') + 1)
+    print(new_str)
+
+
+s0 = 'A' * 20 + '.' * 30 + '\n' + '.' * 49
+s1 = 'A' * 20 + '\n' + 'B' * 29 + '\n' + '.' * 49
+s2 = 'A' * 20 + '\n' + 'B' * 29 + '\n' + 'A' * 20 + '\n' + '.' * 27 + '\n'
+s3 = 'A' * 20 + '\n' + 'B' * 29 + '\n' + 'A' * 20 + '\n' + 'C' * 27 + '\n'
+print(s0)
+time.sleep(1)
+update_screen(s0, s1)
+time.sleep(1)
+update_screen(s1, s2)
+time.sleep(1)
+update_screen(s2, s3)
+
+#print('\n\n\n' + s2)
 
 
 
