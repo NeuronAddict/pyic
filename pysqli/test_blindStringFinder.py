8b72536c1f77d99ae1f1ac20eb284a953a291cce
neuronaddict <dev@lacourarie.org>
2019-01-20 02:35:35 +0100

    reformat file.

--- pysqli/test_blindStringFinder.py
+++ pysqli/test_blindStringFinder.py
@@ -3,7 +3,6 @@ from unittest import TestCase
 import requests
 
 from pysqli import BlindTester, BlindStringFinder, SqliEncoder
-from pysqli.loggers import HttpLogger
 
 
 class TestBlindStringFinder(TestCase):
@@ -18,11 +17,11 @@ class TestBlindStringFinder(TestCase):
 
     def test_str_length(self):
         self.assertEqual(self.string_finder.str_length("(SELECT {})".format(SqliEncoder.str_to_hexa('coucou'))),
-                          6)
+                         6)
 
     def test_read_string(self):
         self.assertEqual(self.string_finder.read_string("(SELECT {})".format(SqliEncoder.str_to_hexa('coucou'))),
-                          'coucou')
+                         'coucou')
 
     def test_read_file(self):
         self.assertEqual(self.string_finder.read_file('/var/www/html/flag.txt'), 'dee8cha8moh9ahyeM4atheemi\n')
