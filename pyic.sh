dd87ded587d90763b7f0342ee7459839417497cf
neuronaddict <dev@lacourarie.org>
2019-05-30 22:27:16 +0200

    script support on pyic.sh

--- pyic.sh
+++ pyic.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-if [[ -d pyic ]]
-then
-    echo "Please launch me on the pyic dir (not pyic/pysqli)"
-    exit 0
-fi
-
 FILE=$(mktemp)
-
-cat ./pysqli/__init__.py ${1} >> ${FILE}
-
-export PYTHONPATH="$PYTHONPATH:."
+cat <<EOF ${1} >> ${FILE}
+from pysqli import *
+EOF
 python3 -i ${FILE}
