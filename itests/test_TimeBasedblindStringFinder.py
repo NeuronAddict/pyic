eecf71fd2a428bf78c572d329878f09c6ca46eda
manufrommars <dev@lacourarie.org>
2019-07-28 16:49:30 +0200

    Add debug for failed test in travis.

--- itests/test_TimeBasedblindStringFinder.py
+++ itests/test_TimeBasedblindStringFinder.py
@@ -4,6 +4,23 @@ import requests
 
 from pyic import BlindTester, BlindStringFinder, SqliEncoder, TimeBlindTester
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+import logging
+import contextlib
+from http.client import HTTPConnection
+
+HTTPConnection.debuglevel = 1
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
 
 class TestBlindStringFinder(TestCase):
     """
