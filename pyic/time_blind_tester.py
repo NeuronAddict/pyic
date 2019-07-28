2acef1454fc33325afdc46f0f2b952dc96dc5d11
neuronaddict <dev@lacourarie.org>
2019-07-28 22:39:57 +0200

    Fix callibrate algorithm.

--- pyic/time_blind_tester.py
+++ pyic/time_blind_tester.py
@@ -69,20 +69,13 @@ class TimeBlindTester:
 
         avg_false /= self.CALIBRATE_TEST_PASSES
         avg_true /= self.CALIBRATE_TEST_PASSES
-
         print('[*] avg False = {}'.format(avg_false))
         print('[*] avg True = {}'.format(avg_true))
 
-        delta_true = max_true - min_true
-        delta_false = max_false - min_false
+        delta = (min_true - max_false)
 
-        print('[*] delta False = {}'.format(delta_false))
-        print('[*] delta True = {}'.format(delta_true))
-
-        delta = max(delta_true / 4, delta_false / 4, min_false / 4, (avg_true - avg_false) / 4)
-
-        self.fmin_true = min_true
-        self.fmax_false = max_false
+        self.fmin_true = min_true - (delta / 3)
+        self.fmax_false = max_false + (delta / 3)
 
         print('[+] fmax_false = {}, fmin_true = {}, delta = {}'.format(delta, self.fmax_false, self.fmin_true))
 
@@ -90,6 +83,6 @@ class TimeBlindTester:
             raise Exception('[-] Callibrate fail, max_false = {} need to be < min_true {}'
                             .format(self.fmin_true, self.fmax_false))
 
-        if abs(self.fmin_true - self.fmax_false) < delta:
-            raise Exception('[-] Callibrate fail, false and true values are too close, delta is too big '
-                            '({}, {}), delta = {})'.format(self.fmin_true, self.fmax_false, delta))
+        # if abs(self.fmin_true - self.fmax_false) < delta:
+        #     raise Exception('[-] Callibrate fail, false and true values are too close, delta is too big '
+        #                     '({}, {}), delta = {})'.format(self.fmin_true, self.fmax_false, delta))
