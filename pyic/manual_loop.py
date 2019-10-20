1ffd43776df250df3b201eb6e0821e93ad0d9fa3
neuronaddict <dev@lacourarie.org>
2019-10-21 00:33:15 +0200

    Delete commands for avoid collisions.

--- pyic/manual_loop.py
+++ pyic/manual_loop.py
@@ -7,11 +7,29 @@ class PayloadCmd(cmd2.Cmd):
     intro = '\n[*] You are entering on payload mode, enter a payload to quick send it via your request builder.\n'
     prompt = colored('payload : >>> ', 'red')
 
-    def __init__(self, request_builder, logger=HttpLogger(), extractor=None):
+    def __init__(self, request_builder, logger=HttpLogger(), extractor=None, delete_set=True, delete_quit=False):
         super().__init__(allow_cli_args=False, allow_redirection=False)
         self.request_builder = request_builder
         self.logger = logger
         self.extractor = extractor
+        del cmd2.Cmd.do_alias
+        del cmd2.Cmd.do_edit
+        del cmd2.Cmd.do_eof
+        del cmd2.Cmd.do_help
+        del cmd2.Cmd.do_macro
+        del cmd2.Cmd.do_py
+        del cmd2.Cmd.do_shortcuts
+        del cmd2.Cmd.do_history
+        del cmd2.Cmd.do_run_pyscript
+        del cmd2.Cmd.do_run_script
+        del cmd2.Cmd.do__relative_run_script
+        del cmd2.Cmd.do_shell
+
+        if delete_quit:
+            del cmd2.Cmd.do_quit
+
+        if delete_set:
+            del cmd2.Cmd.do_set
 
     def do_exit(self, line):
         return True
